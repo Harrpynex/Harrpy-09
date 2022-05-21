57f6d525a6c354078a2cb38eae61eb243dde29aa
Harrpynex <bluestarzgamez@gmail.com>
2022-05-21 10:52:00 +0100

    curriculum vitae

---
+++ node.js
@@ -0,0 +1,68 @@
+
+/*
+sndn neqvurn
+ajrgmwebh h wbjt
+sginqbwmubnq4jehrguer
+k9er0bjmrj ijmwbgjbj90
+rig engb mijb rmkijeroi bjmj
+oi bner kmnjrjebmqjkmahgjejijs8
+kkjk;ei vmertmet
+nje5wwiiomewnrgjw5
+reijmitj5jijewuj5w0
+*/
+
+
+// hbftfudtgtdygrfr
+/*let feeling = prompt("How are you feeling today?").toLowerCase();
+// what do you think the .toLowerCase does at the end?
+
+switch(feeling){
+    case "happy":
+        console.log("Awesome, I'm feeling happy too!");
+        break;
+    case "sad":
+        console.log("That's too bad, I hope you feel better soon.");
+        break;
+    case "hungry":
+        console.log("Me too, let's go eat some pizza!");
+        break;
+    default:
+        console.log("I see. Thanks for sharing!");
+}
+
+
+
+
+
+2 == "2"
+// true
+
+2 === 2
+// true
+
+10 % 3
+// 1
+
+10 % 3 === 1
+// true
+
+true && false
+// false
+
+false || true
+// true
+
+true || false
+// true
+
+let isLearning = true
+if(isLearning){
+console.log("keep it up!");
+} else {
+console.log("pretty sure you are learning...");
+}
+// keep it up!
+// because == compares the value not datatype
+// because ternary operators were used.
+
+*/
